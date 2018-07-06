30a92af8b90a574d603cabfc126fd66d3353b7ca
jjian4 <jjian@umich.edu>
2018-07-05 23:27:03 -0400

    Made PhrasesActivity, added button to new activity

--- app/src/main/java/jameskjiang/com/sleepreminders/MainActivity.kt
+++ app/src/main/java/jameskjiang/com/sleepreminders/MainActivity.kt
@@ -1,12 +1,34 @@
 package jameskjiang.com.sleepreminders
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Go to list of phrases
+        button_goto_phrases.setOnClickListener {
+            val intent: Intent = Intent(this, PhrasesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
+/* PLAN:
+Create Phrases class
+Make list in PhrasesActivity
+Allow user to add phrases as well as edit existing ones
+
+Create Time class
+Create AddTimeActivity
+Allow user to add and edit times
+Display list of times in ActivityMain
+
+Set up notifications
+ */
---
+++ app/src/main/java/jameskjiang/com/sleepreminders/PhrasesActivity.kt
@@ -0,0 +1,12 @@
+package jameskjiang.com.sleepreminders
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PhrasesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phrases)
+    }
+}
